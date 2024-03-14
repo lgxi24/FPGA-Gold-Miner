b79ed02736a8bae6afc97548fc1544e2a7c8e5fa
Guanxi Lu <lambert.lgx@gmail.com>
2024-03-14 14:51:11 +0000

    tidy up folder

---
+++ Golden_Top/software/coursework_v1/obj/default/hello_world_small.d
@@ -0,0 +1,54 @@
+obj/default/hello_world_small.o: hello_world_small.c \
+ ../coursework_v1_bsp//HAL/inc/sys/alt_stdio.h \
+ ../coursework_v1_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../coursework_v1_bsp//HAL/inc/io.h \
+ ../coursework_v1_bsp//HAL/inc/alt_types.h ../coursework_v1_bsp/system.h \
+ ../coursework_v1_bsp/linker.h \
+ ../coursework_v1_bsp//drivers/inc/altera_up_avalon_accelerometer_spi.h \
+ ../coursework_v1_bsp//HAL/inc/alt_types.h \
+ ../coursework_v1_bsp//HAL/inc/sys/alt_dev.h \
+ ../coursework_v1_bsp//HAL/inc/sys/alt_llist.h \
+ ../coursework_v1_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../coursework_v1_bsp//drivers/inc/altera_up_avalon_accelerometer_spi_regs.h \
+ ../coursework_v1_bsp//drivers/inc/altera_avalon_timer_regs.h \
+ ../coursework_v1_bsp//drivers/inc/altera_avalon_timer.h \
+ ../coursework_v1_bsp//HAL/inc/sys/alt_warning.h \
+ ../coursework_v1_bsp//HAL/inc/sys/alt_irq.h \
+ ../coursework_v1_bsp//HAL/inc/nios2.h \
+ ../coursework_v1_bsp//HAL/inc/priv/alt_legacy_irq.h
+
+../coursework_v1_bsp//HAL/inc/sys/alt_stdio.h:
+
+../coursework_v1_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../coursework_v1_bsp//HAL/inc/io.h:
+
+../coursework_v1_bsp//HAL/inc/alt_types.h:
+
+../coursework_v1_bsp/system.h:
+
+../coursework_v1_bsp/linker.h:
+
+../coursework_v1_bsp//drivers/inc/altera_up_avalon_accelerometer_spi.h:
+
+../coursework_v1_bsp//HAL/inc/alt_types.h:
+
+../coursework_v1_bsp//HAL/inc/sys/alt_dev.h:
+
+../coursework_v1_bsp//HAL/inc/sys/alt_llist.h:
+
+../coursework_v1_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../coursework_v1_bsp//drivers/inc/altera_up_avalon_accelerometer_spi_regs.h:
+
+../coursework_v1_bsp//drivers/inc/altera_avalon_timer_regs.h:
+
+../coursework_v1_bsp//drivers/inc/altera_avalon_timer.h:
+
+../coursework_v1_bsp//HAL/inc/sys/alt_warning.h:
+
+../coursework_v1_bsp//HAL/inc/sys/alt_irq.h:
+
+../coursework_v1_bsp//HAL/inc/nios2.h:
+
+../coursework_v1_bsp//HAL/inc/priv/alt_legacy_irq.h:
--- README.md
+++ README.md
@@ -2,6 +2,39 @@
 ### Introduction
 This is the github repository for the ELEC50009 - Information module coursework. FPGA Gold Miner is an FPGA-based multiplayer interactive game. Utilizing the DE10-Lite FPGA board and NIOS II softcore, along with server-client communication based on AWS, it enables two players, using DE10 as gamepads, to enjoy the classic Gold Miner game.
 
+### Structure
+```shell
+FPGA-Gold_Miner/
+│
+├── src/ # game release
+|   ├── picture/  # pictures used in game
+|   ├── coursework_v1.elf # software project
+|   ├── DE10_LITE_Golden_Top.sof # NIOS hardware bit-stream file
+|   ├── gamebox.py # gamebox library
+|   ├── Gold-MinerBasicClient.py # Game file and Client script
+|   ├── host.py # UART communication with FPGA
+|   └── tcpserver.py # Server script
+│
+├── Golden_Top/ # FPGA project
+│   ├── ... 
+│   ├── software/
+|   │   ├── coursework_v1
+|   |   │   ├── coursework_v1.elf # software project
+|   |   │   ├── hello_world_small.c # software program of NIOS
+|   |   │   └── ... 
+|   |   └── coursework_v1_bsp # board support package for the project
+│   ├── ... 
+│   ├── coursework.qsys # QSYS HDL file
+|   ├── coursework.qsys # SOPC Builder information file of the hardware
+│   ├── DE10_LITE_Golden_Top.qpf # Quartus Project
+|   ├── DE10_LITE_Golden_Top.sof # NIOS hardware bit-stream file
+│   └── DE10_LITE_Golden_Top.v # Top level Verilog file
+│
+├── .gitignore
+|
+└── README.md
+```
+
 Create connection between `test.py` script and DE10-Lite
 ```powershell
 # Open NIOS II command shell
